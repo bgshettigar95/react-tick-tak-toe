96aa26e88cb601a5d50946a805b4d5d237cff5dc
Bhagyashree Shettigar <bgshettigar95@gmail.com>
2023-08-12 19:22:43 +0530

    final commit

---
+++ src/components/Board.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import Square from './Square'
+
+const Board = ({ squares, isNextX, onPlay }) => {
+
+    const calculateWinner = (squares) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
+    }
+
+    const handleClick = (i) => {
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        const newSquares = squares.slice();
+        newSquares[i] = isNextX ? 'X' : 'O';
+        onPlay(newSquares);
+    }
+
+    const winner = calculateWinner(squares);
+    let status = '';
+    if (winner) {
+        status = 'Winner ' + winner;
+    } else {
+        status = 'Next turn ' + (!isNextX ? 'X' : 'O');
+    }
+
+
+    return (
+        <div>
+            <h2>{status}</h2>
+            <div className='board'>
+                <div className='board-row'>
+                    <Square value={squares[0]} handleClick={() => handleClick(0)} />
+                    <Square value={squares[1]} handleClick={() => handleClick(1)} />
+                    <Square value={squares[2]} handleClick={() => handleClick(2)} />
+                </div>
+                <div className='board-row'>
+                    <Square value={squares[3]} handleClick={() => handleClick(3)} />
+                    <Square value={squares[4]} handleClick={() => handleClick(4)} />
+                    <Square value={squares[5]} handleClick={() => handleClick(5)} />
+                </div>
+                <div className='board-row'>
+                    <Square value={squares[6]} handleClick={() => handleClick(6)} />
+                    <Square value={squares[7]} handleClick={() => handleClick(7)} />
+                    <Square value={squares[8]} handleClick={() => handleClick(8)} />
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Board
---
+++ src/components/Game.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import Board from './Board'
+
+const Game = () => {
+    const [isNextX, setIsNextX] = useState(true);
+    const [history, setHistory] = useState([Array(9).fill(null)]);
+    const [currentMove, setCurrentMove] = useState(0);
+
+    const onPlay = (newSquares) => {
+        setHistory([...history.slice(0, currentMove + 1), newSquares]);
+        setCurrentMove(currentMove + 1);
+        setIsNextX(!isNextX);
+    }
+
+    const jumpTo = (move) => {
+        setCurrentMove(move);
+    }
+
+    let currentSquares = Array(9).fill(null);
+    if (currentMove > 0) {
+        currentSquares = history[currentMove];
+    }
+
+    const moves = history.map((move, i) => {
+        if (i === 0) {
+            return <li><button onClick={() => jumpTo(i)}>Go to game start</button></li>
+        } else {
+            return <li><button onClick={() => jumpTo(i)}>Go to move {i}</button></li>
+        }
+    });
+
+    return (
+        <div className="game">
+            <Board squares={currentSquares} isNextX={isNextX} onPlay={onPlay} />
+            <div>
+                <ul className='game-moves'>
+                    {moves}
+                </ul>
+
+            </div>
+        </div>
+    )
+}
+
+export default Game
